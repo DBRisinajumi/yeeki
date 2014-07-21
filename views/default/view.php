f17e730faf9fd57e56aec37569a90019b9dfebc9
ivarsju <ivars.ju@gmail.com>
2014-07-21 19:00:36 +0300

    add access filters

--- views/default/view.php
+++ views/default/view.php
@@ -1,15 +1,19 @@
 <div>
+    <?php
+    if (Yii::app()->user->checkAccess("WikiEditor")) { ?>
 	<div class="wiki-controls">
 		<?php echo CHtml::link(Yii::t('wiki', 'Edit'), array('edit', 'uid' => $page->getWikiUid()))?>
 		<?php echo CHtml::link(Yii::t('wiki', 'History'), array('history', 'uid' => $page->getWikiUid()))?>
 	</div>
+    <?php } ?>
 	<div class="wiki-text">
 		<?php echo $text?>
 	</div>
 	<div class="wiki-controls">
+        <?php if (Yii::app()->user->checkAccess("WikiEditor")) { ?>
 		<?php echo CHtml::link(Yii::t('wiki', 'Edit'), array('edit', 'uid' => $page->getWikiUid()))?>
 		<?php echo CHtml::link(Yii::t('wiki', 'History'), array('history', 'uid' => $page->getWikiUid()))?>
-
+        <?php } ?>
 		<?php echo CHtml::link(Yii::t('wiki', 'Page Index'), array('pageIndex'))?>
 	</div>
 </div>
