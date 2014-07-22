80a1b9f7f102a926a01b92de51568074e4823ceb
ivarsju <ivars.ju@gmail.com>
2014-07-22 05:10:00 +0300

    add access filters

--- views/default/no_page.php
+++ views/default/no_page.php
@@ -1,2 +1,6 @@
-<?php echo Yii::t('wiki', 'There is no page yet.')?> 
-<?php echo CHtml::link(Yii::t('wiki', 'Create'), array('edit', 'uid' => $uid))?>?
+<?php
+echo Yii::t('wiki', 'There is no page yet.');
+if (Yii::app()->user->checkAccess("WikiEditor")) {
+    echo ' ' . CHtml::link(Yii::t('wiki', 'Create'), array('edit', 'uid' => $uid)) . '?';
+}
+?>
